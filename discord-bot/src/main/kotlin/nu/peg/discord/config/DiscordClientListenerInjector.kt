7b06923012c679eb4b09636498123bd5b2b2bfbe
Joel Messerli <hi.github@peg.nu>
2018-03-30 18:06:41 +0200

    Fix DiscordClientListenerInjector for tests (no-bot Profile)

--- discord-bot/src/main/kotlin/nu/peg/discord/config/DiscordClientListenerInjector.kt
+++ discord-bot/src/main/kotlin/nu/peg/discord/config/DiscordClientListenerInjector.kt
@@ -2,11 +2,13 @@ package nu.peg.discord.config
 
 import nu.peg.discord.util.DiscordClientListener
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import sx.blah.discord.api.IDiscordClient
 import javax.annotation.PostConstruct
 
 @Component
+@Profile("!no-bot")
 class DiscordClientListenerInjector @Autowired constructor(
         private val discordClient: IDiscordClient,
         private val listeners: List<DiscordClientListener>
