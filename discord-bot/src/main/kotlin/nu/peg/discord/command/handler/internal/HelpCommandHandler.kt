9a26ad04a42d7885a7dc5f50c191ace939e2bbfe
Joel Messerli <hi.github@peg.nu>
2018-05-10 16:33:04 +0200

    Add help command

---
+++ discord-bot/src/main/kotlin/nu/peg/discord/command/handler/internal/HelpCommandHandler.kt
@@ -0,0 +1,26 @@
+package nu.peg.discord.command.handler.internal
+
+import nu.peg.discord.command.Command
+import nu.peg.discord.command.handler.CommandHandler
+import org.springframework.stereotype.Component
+import javax.inject.Inject
+
+@Component
+class HelpCommandHandler : CommandHandler {
+    @Inject
+    private lateinit var commands: List<CommandHandler>
+
+    override fun isAdminCommand() = false
+
+    override fun getNames() = listOf("help", "h", "?", "commands")
+
+    override fun handle(command: Command) {
+        val commandString = commands
+                .sortedBy { it.getNames().sortedByDescending { it.length }.first() }
+                .sortedBy { it.isAdminCommand() }
+                .map { "${it.getNames().joinToString(", ")} ${if (it.isAdminCommand()) "(Admin only)" else ""}" }
+                .joinToString("\n")
+
+        command.message.reply("Commands:\n" + commandString)
+    }
+}
